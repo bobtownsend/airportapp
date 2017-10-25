a8fadfb2a93bf15ca957bb4f98221e978ba8ba6d
Lovie Bates <loviedrives@gmail.com>
2017-10-25 13:32:20 -0700

    hey bob I did the profile page.

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,67 +1,96 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { DefaultPlayer as Video } from 'react-html5video';
-import * as actions from '../actions';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { DefaultPlayer as Video } from "react-html5video";
+import * as actions from "../actions";
+import PropTypes from "prop-types";
+import profile from "./profile";
 
 class Homepage extends Component {
   static contextTypes = {
     router: PropTypes.object
   };
 
-  componentDidMount () {
-    this.props.fetchMessage()
-  };
+  componentDidMount() {
+    this.props.fetchMessage();
+  }
 
-  renderLinks () {
+  renderLinks() {
     if (this.props.authenticated) {
       // show a link for user to go to Dashboard or Sign Out
       return [
-        <li className='nav-item' key={1}>{this.props.message}
-        </li>
-        ,
-        <li className='nav-item' key={2}>
-          <Link className='btn btn-default tg-login__btn' to='/dashboard'>Dashboard</Link>
-        </li>
-        ,
-        <li className='nav-item' key={3}>
-          <Link className='nav-link' to='/signout'>Sign Out</Link>
+        <li className="nav-item" key={1}>
+          {this.props.message}
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="btn btn-default tg-login__btn" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>,
+        <li className="nav-item" key={3}>
+          <Link className="nav-link" to="/signout">
+            Sign Out
+          </Link>
         </li>
       ];
     } else {
       // show a link for user to Sign In or Sign Up
       return [
-        <li className='nav-item'>
-          <Link className='btn btn-default tg-login__btn' to='/signin' key={1}>Sign In</Link>
-        </li>
-        ,
-        <li className='nav-item' key={2}>
-          <Link className='btn btn-default tg-login__btn' to='/signup'>Sign Up</Link>
+        <li className="nav-item">
+          <Link className="btn btn-default tg-login__btn" to="/signin" key={1}>
+            Sign In
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="btn btn-default tg-login__btn" to="/signup">
+            Sign Up
+          </Link>
         </li>
       ];
     }
-  };
+  }
 
-  render () {
+  render() {
+    let person = {
+      name: "joe do",
+      email: "joedow@gmail.com",
+      phone: "555555555"
+    };
     return (
-      <div className='tg-header'>
+      <div className="tg-header">
         <nav className="navbar navbar-default">
           <div className="container-fluid">
             <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
                 <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
+                <span className="icon-bar" />
               </button>
               <a className="navbar-brand" href="#">
-              <strong>App Name Here</strong></a>
+                <strong>App Name Here</strong>
+              </a>
             </div>
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div
+              className="collapse navbar-collapse"
+              id="bs-example-navbar-collapse-1"
+            >
               <ul className="nav navbar-nav">
-                <li className="active"><a href="/">Home <span className="sr-only">(current)</span></a></li>
-                <li><a href="#">Tab 2</a></li>
-                <li><a href="#">Tab 3</a></li>
-
+                <li className="active">
+                  <a href="/">
+                    Home <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/Profile">Profile</a>
+                </li>
+                <li>
+                  <a href="/AboutUs">AboutUs</a>
+                </li>
               </ul>
               <ul className="nav navbar-nav navbar-right">
                 {this.renderLinks()}
@@ -70,68 +99,101 @@ class Homepage extends Component {
           </div>
         </nav>
 
+        {/* -----------------------------------------------BODY-----------------------------------------*/}
 
+      
 
-        <footer className='page-footer center-on-small-only'>
-          <div className='container-fluid'>
-            <div className='row'>
-              <div className='col-lg-3 col-md-6 ml-auto'>
-                <h5 className='title mb-3'><strong>About Us</strong></h5>
+        {/* -----------------------------------------------BODY-----------------------------------------*/}
+
+        <footer className="page-footer center-on-small-only">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 ml-auto">
+                <h5 className="title mb-3">
+                  <strong>About Us</strong>
+                </h5>
                 <p>Designed by greatness.</p>
                 <p>Designed for greatness.</p>
               </div>
 
-              
-              <hr className='w-100 clearfix d-sm-none' />
-              <div className='col-lg-2 col-md-6 ml-auto'>
-                <h5 className='title mb-3'><strong>First column</strong></h5>
+              <hr className="w-100 clearfix d-sm-none" />
+              <div className="col-lg-2 col-md-6 ml-auto">
+                <h5 className="title mb-3">
+                  <strong>First column</strong>
+                </h5>
                 <ul>
-                  <li className='nav-item'><a href='/'>Home</a></li>
-                  <li className='nav-item'><a href='/signup'>Sign Up</a></li>
-                  <li className='nav-item'><a href='/signin'>Sign In</a></li>
-                  <li className='nav-item'><a href='/signout'>Sign Out</a></li>
+                  <li className="nav-item">
+                    <a href="/">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signup">Sign Up</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signin">Sign In</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signout">Sign Out</a>
+                  </li>
                 </ul>
               </div>
 
-
-              <div className='col-lg-2 col-md-6 ml-auto'>
-                <h5 className='title mb-3'><strong>Second column</strong></h5>
+              <div className="col-lg-2 col-md-6 ml-auto">
+                <h5 className="title mb-3">
+                  <strong>Second column</strong>
+                </h5>
                 <ul>
-                <li className='nav-item'><a href='/'>Home</a></li>
-                <li className='nav-item'><a href='/signup'>Sign Up</a></li>
-                <li className='nav-item'><a href='/signin'>Sign In</a></li>
-                <li className='nav-item'><a href='/signout'>Sign Out</a></li>
-              </ul>
+                  <li className="nav-item">
+                    <a href="/">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signup">Sign Up</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signin">Sign In</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signout">Sign Out</a>
+                  </li>
+                </ul>
               </div>
 
-
-              <div className='col-lg-2 col-md-6 ml-auto'>
-                <h5 className='title mb-3'><strong>Third column</strong></h5>
+              <div className="col-lg-2 col-md-6 ml-auto">
+                <h5 className="title mb-3">
+                  <strong>Third column</strong>
+                </h5>
                 <ul>
-                <li className='nav-item'><a href='/'>Home</a></li>
-                <li className='nav-item'><a href='/signup'>Sign Up</a></li>
-                <li className='nav-item'><a href='/signin'>Sign In</a></li>
-                <li className='nav-item'><a href='/signout'>Sign Out</a></li>
-              </ul>
+                  <li className="nav-item">
+                    <a href="/">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signup">Sign Up</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signin">Sign In</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/signout">Sign Out</a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-          <div className='footer-copyright'>
-            <div className='container-fluid'>
-            © 2017 Copyright: <a href='#'> Dylan.me </a>
+          <div className="footer-copyright">
+            <div className="container-fluid">
+              © 2017 Copyright: <a href="#"> Dylan.me </a>
             </div>
           </div>
         </footer>
-    </div>
+      </div>
     );
   }
-};
+}
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     authenticated: state.auth.authenticated,
     message: state.auth.message
   };
-};
+}
 
 export default connect(mapStateToProps, actions)(Homepage);
