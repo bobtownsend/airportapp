a425a9bc7bdfba4ce2b7b6d6f6ed7f484b1f44a1
Lovie Bates <loviedrives@gmail.com>
2017-11-06 15:24:15 -0700

    add events

--- client/src/components/eventsCalendar.js
+++ client/src/components/eventsCalendar.js
@@ -15,5 +15,44 @@ export default [
       'title': 'DTS STARTS',
       'start': new Date(2017, 9, 27, 0, 0, 0),
       'end': new Date(2017, 9, 27, 0, 0, 0)
-    }]
+    },
+
+    {
+      'title': 'Phoenix Fan Fest $15  9am',
+      'start': new Date(2017, 10, 11, 9, 0 ),
+      'end': new Date(2017, 10, 11,),
+      'desc': 'Phoenix Convention Center'
+    },
+
+    {
+      'title': 'Saturday Moring Open Hack  9am',
+      'start': new Date(2017, 10, 11, 9, 0 ),
+      'end': new Date(2017, 10, 11,),
+      'desc': 'GoDaddy Tempe'
+    },
+
+    {
+      'title': 'Harry Potter Family Event $13 10am',
+      'start': new Date(2017, 10, 11, 10, 0 ),
+      'end': new Date(2017, 10, 11,),
+      'desc': '@ Arizona Science Center'
+     
+    },
+
+    {
+      'title': 'Melanie Swan--Payment Channels 11am',
+      'start': new Date(2017, 10, 11, 10, 0 ),
+      'end': new Date(2017, 10, 11,),
+      'desc': '@ GCU Desert Blockchain'
+    },
+
+
+  {
+    'title': 'Hackfest @ DeVry CyberSecurity Range 11am',
+    'start': new Date(2017, 10, 11, 10, 0 ),
+    'end': new Date(2017, 10, 11,),
+    'desc': '@ Devry Linux Hackfest'
+  },
   
+
+ ]
