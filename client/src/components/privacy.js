fb66ab17002e82380b974bf97c85ab062c0244f5
Ray Roman <rayroman0809@gmail.com>
2017-11-09 14:14:13 -0700

    more styling

--- client/src/components/privacy.js
+++ client/src/components/privacy.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import '../css/style.css';
 
 
 class privacy extends Component {
@@ -12,8 +13,22 @@ class privacy extends Component {
 <br></br>
 <br></br>
 <br></br>
-<h1>Welcome To My Page</h1>
+<div id="privacy" className="privacy">
+<div>
+     <h2>Privacy</h2>
+        <div>
+            <div>
+                <p id="privacyP" className="privacyP">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+        </div>
+    </div>
+</div><br></br>
+
+<div id="privacyPP" className="privacyPP">
+<h2>Terms & Conditions</h2>
+<p id="privacyPP" className="privacyPP">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
 </div>
+</div>       
         );
     }
 }
